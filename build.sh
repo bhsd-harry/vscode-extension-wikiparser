4f4ce76d8cee9a9759b7d1b38eb910ac8358a0d4
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-06-21 12:08:55 +0800

    chore(package.json): upgrade

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/usr/local/bin/bash
 npx tsc --project server/tsconfig.json && \
-npx esbuild server/src/lsp.ts --charset=utf8 --bundle --target=es2023 --platform=node --outdir=server/dist\
+npx esbuild server/src/lsp.ts --charset=utf8 --bundle --target=es2024 --platform=node --outdir=build\
  --external:chalk --external:entities --external:stylelint --external:mathjax\
  --external:vscode-languageserver --external:vscode-languageserver-textdocument\
- --external:vscode-css-languageservice --external:vscode-json-languageservice --external:vscode-html-languageservice
+ --external:vscode-css-languageservice --external:vscode-json-languageservice --external:vscode-html-languageservice && \
+npx esbuild build/lsp.js --charset=utf8 --minify --target=es2023 --outdir=server/dist
 mkdir -p server/config
 WIKILINT=$(node -e 'console.log( path.resolve( require.resolve( "wikilint/package" ), ".." ) )')
-cp -r "$WIKILINT"/config .
+cp -r "$WIKILINT"/{config,data} .
 mv config/default.json server/config/
-cp -r "$WIKILINT"/data .
-rm config/.schema.json data/.schema.json
+rm {config,data}/.schema.json
